9c2f0a507f5da5848da5a4ad25fea0796c2d17bc
elh-art <code.enfaced@aleeas.com>
2022-09-06 18:43:17 +0200

    first commit

---
+++ src/components/TopMenu.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const TopMenu = () => {
+  return (
+    <>
+     <nav className="top-menu bg-theme">
+      <div className="container d-flex justify-content-between align-items-center">
+        <div className="site-settings d-flex">
+          <select name="language">
+            <option value="language">ENG</option>
+            <option value="language">SWE</option>
+          </select>
+          <select name="currency">
+            <option value="currency">SEK</option>
+            <option value="currency">EUR</option>
+          </select>
+        </div>
+        <div className="links d-flex justify-content-end align-items-center">
+          <div className="links-top">
+            <a href="#">Newsletter</a> 
+            <a href="#">Contact us</a>
+            <a href="#">FAQs</a>
+          </div>
+          <div className="socialmedia">
+            <a href="#"><i className="fa-brands fa-twitter"></i></a>
+            <a href="#"><i className="fa-brands fa-facebook"></i></a>
+            <a href="#"><i className="fa-brands fa-linkedin"></i></a>
+            <a href="#"><i className="fa-brands fa-tiktok"></i></a>
+          </div>
+        </div>
+      </div>
+     </nav>
+    </>
+  )
+}
+
+export default TopMenu
